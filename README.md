1ae562c1822a25b16ff911012639682b92b68d48
Muhammad Raiyaan <83336994+muhammedraiyaan2@users.noreply.github.com>
2021-11-04 20:27:11 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# chat-application
-This application will allow you to chat with many people there is no limit
+# Chat Application
+This chat application will help you to chat each other there is no limit of number of person you can chat up to trillions of people
+
+[Click here to open chat application](https://muhammedraiyaan2.github/chat-application)
+# About us and Contact us
+If you want to know about us and contact us then go to [This Site](https://muhammedraiyaan2.github/Profile)
---
+++ index.js
@@ -0,0 +1,45 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD3pNXQ8-0wZProhU2zFTg3DFFyzLn97xI",
+    authDomain: "chatapplication2-a094c.firebaseapp.com",
+    databaseURL: "https://chatapplication2-a094c-default-rtdb.firebaseio.com/",
+    projectId: "chatapplication2-a094c",
+    storageBucket: "chatapplication2-a094c.appspot.com",
+    messagingSenderId: "1083773191735",
+    appId: "1:1083773191735:web:20799b6f1ca1cb243161b2"
+};
+firebase.initializeApp(firebaseConfig);
+if (localStorage.getItem("Name") === null || localStorage.getItem("Name") == `` || localStorage.getItem("Name") == null) {
+    let lName = prompt("Please enter your name to display the name in the message")
+    localStorage.setItem("Name", lName)
+}
+let messages=document.getElementById("message")
+let btn = document.getElementById("btn")
+let container = document.getElementById("container")
+let date=new Date()
+messages.addEventListener("keydown",function(event) {
+    if(event.keyCode==13){
+        btn.click();
+    }
+})
+let t=`${date.getHours()}: ${date.getMinutes()}: ${date.getSeconds()}`
+btn.addEventListener("click",function(){
+    firebase.database().ref("Chats/"+Math.floor(Math.random()*9999999999999999)).set({
+        Name:localStorage.getItem("Name"),
+        Chat:messages.value,
+        Time:t,
+    })
+    location.reload()
+})
+firebase.database().ref("Chats").once("value",function(snapshot){
+    let data=snapshot.val()
+    setInterval(() => {
+        data=snapshot.val()
+    }, 1);
+    for(let i in data){
+        container.innerHTML+=`<div class="message">${data[i].Name}: ${data[i].Chat} <br>${data[i].Time}</br></div>`
+    }
+})
+let clear=document.getElementById("clear").onclick=function(){
+    firebase.database().ref("Chats").remove()
+    location.reload()
+}
